ecfd0c0443310ccf0e06832b3a08c496a7a35ca5
Eduardo Felipe Hammes <eduaardohammes03@gmail.com>
2026-02-06 21:43:17 -0300

    Iniciando entendimento sobre funcoes

---
+++ funcoes.js
@@ -0,0 +1,16 @@
+// testando funções
+
+function saudacao(nome) {
+    console.log('Olá,', nome)
+}
+
+saudacao('Eduardo')
+saudacao('Jéssica')
+saudacao('Nathália')
+
+function calcularDobroDeUm(numero){
+    return numero * 2
+}
+
+const numeroDobrado = calcularDobroDeUm(5)
+console.log('O dobro de 5 é', numeroDobrado)
