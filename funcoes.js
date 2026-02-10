c3ed9848a8ac42dc7ad1b3177e669908d9cbab56
Eduardo Felipe Hammes <eduaardohammes03@gmail.com>
2026-02-09 23:22:58 -0300

    Iniciando conceitos de arrow functions

--- funcoes.js
+++ funcoes.js
@@ -1,16 +1,31 @@
 // testando funções
 
-function saudacao(nome) {
-    console.log('Olá,', nome)
-}
+// function saudacao(nome) {
+//    console.log('Olá,', nome)
+// }
+
+// arrow function
+// const saudacao = (nome) => {
+//     console.log('Vida longa e próspera,', nome)
+// }
+
+// arrow function v2 - usar apenas quando tiver só um parametro
+const saudacao = nome => console.log('Olá,', nome)
 
 saudacao('Eduardo')
 saudacao('Jéssica')
 saudacao('Nathália')
 
-function calcularDobroDeUm(numero){
-    return numero * 2
-}
+// function calcularDobroDeUm(numero){
+//     return numero * 2
+// }
+
+// const calcularDobroDeUm = (numero) => {
+//     return numero * 2
+// }
+
+// arrow function - não precisa do return em arrow function, será retornado por padrão
+const calcularDobroDeUm = numero => numero * 2
 
 const numeroDobrado = calcularDobroDeUm(5)
 console.log('O dobro de 5 é', numeroDobrado)
